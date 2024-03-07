043bf6b31a4fc29136ca79c6409c4850556ae96f
AMIT SHUKLA <amit800blp@gmail.com>
2024-03-07 15:34:12 +0530

    connect with database

---
+++ shop/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to='category')
+
+    def __str__(self):
+        return self.title
