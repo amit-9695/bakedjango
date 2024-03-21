e8a6d3ebfb1561b782f93d89916eb0df69ea6a42
AMIT SHUKLA <amit800blp@gmail.com>
2024-03-21 15:39:57 +0530

    Add Template

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',TemplateView.as_view(template_name='index.html'), name='home'),
+    path('page',TemplateView.as_view(template_name='page1.html'), name='page1'),
 ]
 
 if settings.DEBUG:
