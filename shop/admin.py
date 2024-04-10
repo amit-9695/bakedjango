585e02bf15db928085e22483e79f12382a8711b9
AMIT SHUKLA <amit800blp@gmail.com>
2024-04-10 21:03:03 +0530

    add Dish &Category Modules by which we can add dishes in database from the admin dashboard

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Category,Contact
+from .models import Category,Contact,Dish
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Contact)
+admin.site.register(Dish)
--- shop/models.py
+++ shop/models.py
@@ -5,6 +5,7 @@ class Category(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     image = models.ImageField(upload_to='category')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
@@ -18,4 +19,23 @@ class Contact(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.name 
+    
+class Dish(models.Model):
+        dish_type_choices = (
+            ('egg', 'Egg'),
+            ('vegan', 'Vegan'),
+            
+        )
+        title = models.CharField(max_length=50)
+        image = models.ImageField(upload_to='dish')
+        description = models.TextField()
+        price = models.DecimalField(max_digits=10, decimal_places=2)
+        category = models.ForeignKey(Category, on_delete=models.CASCADE)
+        dish_type = models.CharField(max_length=10, choices=dish_type_choices)
+        avaliable = models.BooleanField(default=True)
+        best_seller = models.BooleanField(default=False)
+        created_at = models.DateTimeField(auto_now_add=True)
+
+        def __str__(self):
+            return self.title
